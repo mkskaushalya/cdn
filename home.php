14d3187ccfe7020a3c24126a3895228dbe654bdc
Sahan Kaushalya <mkskaushalya@gmail.com>
2022-10-30 06:26:53 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,76 @@
+<div class="home-bg"></div>
+<section id="home" class="section home">
+ <div class="overlay">
+    <h2>ICE at SLTC</h2>
+    <p>We are a growing community formed by the students of Sri Lanka Technological Campus of Padukka following the degree program of BSc. (Hons) Engineering in Information and Communication Engineering. Our idea of a community is one where all members get to develop their skills at their own pace by contributing to our projects.</p>
+ </div>
+</section>
+<section class="vision" id="vision">
+    <div class="vision-cont">
+        <h2>Vision</h2>
+        <p>Our vision: Securing a bright future for all ICE undergraduates of SLTC by working in competitive business environments.</p>
+        <h2 style="margin-top:1rem;">Mission</h2>
+        <p>Our vision: Securing a bright future for all ICE undergraduates of SLTC by working in competitive business environments.</p>
+    </div>
+    <a href="#">
+        <button>Learn More...</button>
+    </a>
+</section>
+<section id="aim" class="section aim">
+<div class="aim-bg"></div>
+ <div class="aim-overlay">
+    <h2>Aims and Objectives</h2>
+    <p>We are a growing community formed by the students of Sri Lanka Technological Campus of Padukka following the degree program of BSc. (Hons) Engineering in Information and Communication Engineering. Our idea of a community is one where all members get to develop their skills at their own pace by contributing to our projects.</p>
+ </div>
+</section>
+<section class="projects">
+    <h2>Current Projects</h2>
+    <div class="project-cont">
+
+        <div class="project-card">
+            <div class="proj-img">
+                <img src="img/aim-bg.jpg" alt="">
+            </div>
+            <div class="describe">
+                <h3>Project Title</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad aliquam inventore, ut in provident vel nobis, voluptate omnis assumenda totam laborum, repudiandae id! Veritatis voluptas sunt nam deserunt. Blanditiis, a!</p>
+                <a href="#">Learn More</a>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <div class="proj-img">
+                <img src="img/aim-bg.jpg" alt="">
+            </div>
+            <div class="describe">
+                <h3>Project Title</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad aliquam inventore, ut in provident vel nobis, voluptate omnis assumenda totam laborum, repudiandae id! Veritatis voluptas sunt nam deserunt. Blanditiis, a!</p>
+                <a href="#">Learn More</a>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <div class="proj-img">
+                <img src="img/aim-bg.jpg" alt="">
+            </div>
+            <div class="describe">
+                <h3>Project Title</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad aliquam inventore, ut in provident vel nobis, voluptate omnis assumenda totam laborum, repudiandae id! Veritatis voluptas sunt nam deserunt. Blanditiis, a!</p>
+                <a href="#">Learn More</a>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <div class="proj-img">
+                <img src="img/aim-bg.jpg" alt="">
+            </div>
+            <div class="describe">
+                <h3>Project Title</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad aliquam inventore, ut in provident vel nobis, voluptate omnis assumenda totam laborum, repudiandae id! Veritatis voluptas sunt nam deserunt. Blanditiis, a!</p>
+                <a href="#">Learn More</a>
+            </div>
+        </div>
+
+
+    </div>
+</section>
