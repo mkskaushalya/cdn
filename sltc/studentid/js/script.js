08779619cdac06d0af6ef38ae617a7829f586fb4
Sahan Kaushalya <mkskaushalya@gmail.com>
2024-03-12 14:01:55 +0530

    Create script.js

---
+++ sltc/studentid/js/script.js
@@ -0,0 +1,31 @@
+// =============FULL SCREEN============
+
+const fullScreenBtn = document.getElementById('full-screen')
+function getFullscreenElement(){
+    return document.fullscreenElement
+        || document.webkitFullscreenElement
+        || document.mozFullscreenElement
+        || document.msFullscreenElement;
+}
+function toggleFullScreen(){
+    if(getFullscreenElement()){
+        document.exitFullscreen();
+        fullScreenBtn.classList.remove('active-fullscreen');
+
+    }else{
+
+        document.documentElement.requestFullscreen().catch(console.log);
+        fullScreenBtn.classList.add('active-fullscreen');
+    }
+}
+document.addEventListener("dblclick", () =>{
+    toggleFullScreen();
+});
+
+const navbutton = document.querySelectorAll('.nav-button');
+navbutton.forEach(navbuttonel => {
+    navbuttonel.addEventListener('click', () => {
+        document.querySelector(".loader").classList.remove("hidden");
+    });
+});
+
